e0ea214d6a6de95296bae5ef04e8a4e09d3dd4b9
Sheepearrrrrrrrrr <sheepearrr@gmail.com>
2025-04-23 22:43:33 +0200

    Small update to the code.

--- pypypypypypy.py
+++ pypypypypypy.py
@@ -16,66 +16,11 @@ yaw = hub.imu
 base.settings(250)
 
 dis = {
-    Color.RED: (90, True),
-    Color.YELLOW: (90, False),
-    Color.WHITE: (-90, True),
-    Color.GREEN: (-90, False)
+    Color.RED: (90, 100),
+    Color.YELLOW: (90, 50),
+    Color.WHITE: (-90, 100),
+    Color.GREEN: (-90, 50)
 }
-# base.straight(100)
-# for i in range(0, 16):
-#     base.turn(90)
-    # base.straight(100)
-# base.curve(200, 90)
-def main():
-    print('started!')
-    wait(500)
-    yaw.reset_heading(0)
-    base.settings(straight_speed=250)
-    base.straight(350)
-    turnByGyros(-90)
-    base.straight(-300)
-    base.straight(910)
-    turnByGyros(-90)
-    handMotor.run_target(650, -55)
-    base.straight(80)
-    for j in range(0, 3):
-        handMotor.run_angle(650, 26)
-        if (j != 2):
-            base.straight(15)
-    handMotor.run_angle(650, -45)
-    base.straight(-120)
-    turnByGyros(-90)
-    handMotor.run_target(650, -55)
-    base.straight(-300)
-    base.straight(615)
-    handMotor.run_angle(650, 20)
-    turnByGyros(90)
-    handMotor.run_target(650, -50)
-    #blue water ball
-    base.straight(100)
-    handMotor.run_target(650, -35)
-    base.straight(-100)
-    #end
-    #straigthen
-    turnByGyros(90)
-    base.straight(-500)
-    #end
-    #back to container
-    base.straight(910)
-    turnByGyros(-90)
-    #end
-    base.straight(120)
-    handMotor.run_target(650, -60)
-    handMotor.run_angle(650, 30)
-    base.straight(-20)
-    handMotor.run_angle(650, -30)
-    turnByGyros(45)
-    base.straight(40)
-    handMotor.run_angle(650, 30)
-    handMotor.run_angle(650, -30)
-    base.straight(-40)
-    turnByGyros(-45)
-    base.straight(50)
 
 def randomization():
     colors = []
@@ -94,9 +39,12 @@ def randomization():
     base.turn(-90)
     base.settings(250)
     for i in range(1, 4):
+        print('starting a bunch')
         currentBunch = i * 2 - 1
+        if colors[currentBunch] == Color.NONE && colors[currentBunch - 1] == Color.NONE:
+            continue
         base.straight(-200)
-        base.straight(230)
+        base.straight(180 + i * 50)
         handMotor.run_target(250, -38)
         base.turn(-60)
         base.straight(75)
@@ -109,15 +57,26 @@ def randomization():
         base.straight(100)
         index = 0
         for j in range(0, 2):
-            shit = dis[]
             shit2 = currentBunch - j
+            if colors[shit2] == Color.NONE:
+                continue
+            shit = dis[shit2]
             base.turn(shit[0])
-            base.straight(100 if shit[1] else 50)
+            base.straight(shit[1])
             base.turn(-shit[0])
             base.straight(100)
-            break
-        break
-    
-    
-#main()
+            '''
+            Arm stuff
+            '''
+            base.straight(-100)
+            base.turn(-shit[0])
+            base.straight(shit[1])
+            base.turn(shit[0])
+        if i <= 2:
+            base.straight(-100)
+            base.turn(-90)
+            base.straight(-350)
+    base.straight(500)
+    print('Probably ended')
+
 randomization()
