881675fe134efd4d6f306993280198e4368c16f2
Sheepearrrrrrrrrr <sheepearrr@gmail.com>
2025-05-13 16:08:34 +0200

    Update pypypypypypy.py

--- pypypypypypy.py
+++ pypypypypypy.py
@@ -52,7 +52,7 @@ def randomization():
         hub.light.on(color)
         wait(100)
     base.turn(-45)
-    base.straight(225)
+    base.straight(220)
     base.turn(-45)
     base.straight(-1075)
     base.straight(1000)
@@ -62,15 +62,16 @@ def randomization():
         if colors[currentBunch] == Color.NONE and colors[currentBunch - 1] == Color.NONE:
             continue
         base.straight(i * -400)
-        
         to_turn = 40
-        base.straight(i * 175)
+        base.straight(185 * i)
         if colors[currentBunch] != Color.NONE:
             rHandMotor.run_target(400, -4, wait=False)
-        lHandMotor.run_target(400, -4)
+        if colors[currentBunch - 1] != Color.NONE:
+            lHandMotor.run_target(400, -4)
         base.turn(-40)
         base.straight(125)
-        lHandMotor.run_target(400, -65)
+        if colors[currentBunch - 1] != Color.NONE:
+            lHandMotor.run_target(400, -65)
         base.straight(-70)
         if colors[currentBunch] != Color.NONE:
             base.curve(144, -30)
@@ -80,19 +81,20 @@ def randomization():
             to_turn = 70
         base.turn(to_turn)
         base.straight(-400 - i * 100)
-
         base.straight(525)
         base.turn(90)
         base.straight(175)
+        skipped = False
         for j in range(0, 2):
             shit2 = currentBunch - j
             if colors[shit2] == Color.NONE:
                 continue
             shit = dis[colors[shit2]]
-            base.turn(shit[0])
-            base.straight(350)
-            base.turn(-shit[0])
-            base.straight(575)
+            if not skipped:
+                base.turn(shit[0])
+                base.straight(350)
+                base.turn(-shit[0])
+                base.straight(600)
             if j == 0:
                 base.turn(0 if shit[1] else -65)
                 rHandMotor.run_target(400, -4)
@@ -100,6 +102,9 @@ def randomization():
                 rHandMotor.run_target(400, -175)
                 base.straight(100)
                 base.turn(0 if shit[1] else 65)
+                if colors[currentBunch - 1] != Color.NONE and (not dis[colors[currentBunch - 1]][1]):
+                    skipped = True
+                    continue
             else:
                 base.turn(-15 if not shit[1] else 65)
                 lHandMotor.run_target(400, -4)
@@ -107,16 +112,13 @@ def randomization():
                 lHandMotor.run_target(400, -175)
                 base.straight(100)
                 base.turn(15 if not shit[1] else -65)
-            base.straight(-550)
+            base.straight(-575)
             base.turn(-shit[0])
             base.straight(375)
             base.turn(shit[0])
         if i <= 2:
             base.straight(-175)
             base.turn(-90)
-    base.settings(650)
-    base.straight(1200)
-    base.straight(-200)
     print(stopwatch.time())
 
 randomization()
